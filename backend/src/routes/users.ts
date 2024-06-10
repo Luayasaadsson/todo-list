f09bbfe2af1680d98a0458c90c13064c2fb7b197
Luay Asaadsson <155851838+Luayasaadsson@users.noreply.github.com>
2024-06-10 22:51:13 +0200

    Todos routes, middleware mapp, updateradd prisma
    - Prisma uppdaterad med createdAt.
    - Flyttade ut auth i en egen Middleware mapp för bättre kodstruktur.
    - Uppdaterade users.ts.
    - Lagt in routes för todos enligt CRUD.

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,8 +1,9 @@
-import { Router, Request, Response, NextFunction } from "express";
+import { Router, Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import { auth, AuthenticatedRequest } from "../middleware/auth";
 dotenv.config();
 
 const router = Router();
@@ -45,13 +46,11 @@ router.post("/login", async (req: Request, res: Response) => {
       return res.status(401).send("Invalid credentials");
     }
 
-    if (!process.env.JWT_SECRET) {
-      return res.status(500).json({ error: "Internal server error" });
-    }
-
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    const secret = process.env.JWT_SECRET || "default_secret";
+    const token = jwt.sign({ id: user.id }, secret, {
       expiresIn: "1h",
     });
+
     res.status(200).json({ message: `Token created: ${token}` });
   } catch (error) {
     console.error(error);
@@ -59,58 +58,38 @@ router.post("/login", async (req: Request, res: Response) => {
   }
 });
 
-interface AuthenticatedRequest extends Request {
-  user?: JwtPayload;
-}
-
-// Middleware för att verifiera att användaren är betrodd
-const auth = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
-  const token = req.headers.authorization?.split(" ")[1];
-
-  if (!token) {
-    return res.status(401).send("Access denied");
-  }
-
-  if (!process.env.JWT_SECRET) {
-    return res.status(500).json({ error: "internal server error" });
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET) as jwt.JwtPayload;
-    req.user = decoded;
-    next();
-  } catch (error) {
-    console.error(error);
-    res.status(400).send("Invalid token");
-  }
-};
-
 // PUT /users/update
-router.put("/update", auth, async (req: AuthenticatedRequest, res: Response) => {
-  const userId = req.user?.id;
-  const { username, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
+router.put(
+  "/update",
+  auth,
+  async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user?.id;
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  try {
-    const updatedUser = await prisma.user.update({
-      where: { id: userId },
-      data: { username, email, password: hashedPassword },
-    });
-    res.status(200).json({ message: "User updated successfully", user: updatedUser });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
+    try {
+      const updatedUser = await prisma.user.update({
+        where: { id: userId },
+        data: { username, email, password: hashedPassword },
+      });
+      res
+        .status(200)
+        .json({ message: "User updated successfully", user: updatedUser });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal server error" });
+    }
   }
-});
+);
 
-// GET /users/:id - Hämtar en specifik användares information
+// GET /users/:id
 router.get("/:id", auth, async (req: AuthenticatedRequest, res: Response) => {
   const userId = parseInt(req.params.id);
 
   try {
     const user = await prisma.user.findUnique({
       where: { id: userId },
-      select: { username: true, email: true, password: true }
+      select: { username: true, email: true, password: true },
     });
 
     if (!user) {
@@ -124,27 +103,34 @@ router.get("/:id", auth, async (req: AuthenticatedRequest, res: Response) => {
   }
 });
 
-
 // DELETE /users/delete
-router.delete("/delete", auth, async (req: AuthenticatedRequest, res: Response) => {
-  const userId = req.user?.id;
+router.delete(
+  "/delete",
+  auth,
+  async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user?.id;
 
-  try {
-    await prisma.user.delete({
-      where: { id: userId },
-    });
-    res.status(200).send("User account deleted successfully");
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
+    try {
+      await prisma.todo.deleteMany({
+        where: { userId: userId },
+      });
+
+      await prisma.user.delete({
+        where: { id: userId },
+      });
+      res
+        .status(200)
+        .send("User account and all related todos deleted successfully");
+    } catch (error) {
+      console.error("Error deleting user or todos:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
   }
-});
-
-
+);
 
 // Protected route
 router.get("/protected", auth, (req: AuthenticatedRequest, res: Response) => {
-  res.send("The user it authenticated and can access this route");
+  res.send("The user is authenticated and can access this route");
 });
 
 export default router;
